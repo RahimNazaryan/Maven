0ccb71ed54bf93d83b976f62705b67158883124c
Rahim <nazaryanrahim@gmail.com>
2021-03-06 13:00:10 -0500

    first commit

---
+++ src/test/java/com/hrms/pages/EmployeeListPage.java
@@ -0,0 +1,20 @@
+package com.hrms.pages;
+
+import com.hrms.util.CommonMethods;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EmployeeListPage extends CommonMethods {
+
+    @FindBy(id = "empsearch_id")
+    public WebElement idEmployee;
+
+    @FindBy(id = "searchBtn")
+    public WebElement searchBtn;
+
+    public EmployeeListPage() {
+        PageFactory.initElements(driver, this);
+    }
+}
---
+++ target/cucumber-default-reports/report.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/login.feature");
+formatter.feature({
+  "name": "Login Functionality",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@featureTag"
+    },
+    {
+      "name": "@login"
+    },
+    {
+      "name": "#"
+    },
+    {
+      "name": "feature"
+    },
+    {
+      "name": "level"
+    },
+    {
+      "name": "tag"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Login with invalid credentials",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@featureTag"
+    },
+    {
+      "name": "@login"
+    },
+    {
+      "name": "#"
+    },
+    {
+      "name": "feature"
+    },
+    {
+      "name": "level"
+    },
+    {
+      "name": "tag"
+    },
+    {
+      "name": "@smoke"
+    },
+    {
+      "name": "@syntax"
+    },
+    {
+      "name": "@regression"
+    },
+    {
+      "name": "@whatever"
+    },
+    {
+      "name": "#"
+    },
+    {
+      "name": "adding"
+    },
+    {
+      "name": "multiple"
+    },
+    {
+      "name": "scenario"
+    },
+    {
+      "name": "level"
+    },
+    {
+      "name": "tags"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "enter invalid credentials",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.hrms.stepdefinitions.LoginStepDefinition.enter_invalid_credentials()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "click on login button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.hrms.stepdefinitions.LoginStepDefinition.click_on_login_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "verify error message",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.hrms.stepdefinitions.LoginStepDefinition.verify_error_message()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.embedding("image/png", "embedded0.png", "Login with invalid credentials");
+formatter.after({
+  "status": "passed"
+});
+});
